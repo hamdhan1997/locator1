938fdf5ed83e6f65f45169fc29dae054df0f63b1
hamdhan1997 <56188981+hamdhan1997@users.noreply.github.com>
2019-10-08 00:32:16 +0700

    Create eror.php

---
+++ server/eror.php
@@ -0,0 +1,14 @@
+<?php
+
+file_put_contents("error.txt", "Error: " . $_GET['error'] . "\n", FILE_APPEND);
+
+header('Location:javascript://history.go(-1)');
+exit();
+?>
+<?php
+
+file_put_contents("error.txt", "Error: " . $_GET['error'] . "\n", FILE_APPEND);
+
+header('Location:javascript://history.go(-1)');
+exit();
+?>
